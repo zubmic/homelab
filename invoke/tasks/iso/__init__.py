f238001d2d43aaab33a7d3c6714351e8aacb2a71
Michał Żuber <michalzuber22@gmail.com>
2022-03-26 12:18:41 +0100

    Create a helper function to remove repetition and apply pylint suggestions

--- invoke/tasks/iso/__init__.py
+++ invoke/tasks/iso/__init__.py
@@ -1,32 +1,32 @@
+"""
+Set of tasks related to iso file
+"""
+from os.path import exists
 from invoke import Collection, task, Result
-from os.path import exists, basename
-from re import split
+import helpers
 
 namespace = Collection()
 
 @task
 def get(context):
-    url = context['iso_url']
-    # Extract file name from url
-    file_name = basename(url)
+    """
+    Downloads a file from url if not already present.
+    """
+    file_path = context['file_dir'] + helpers.iso_name_from_url(context['iso_url'])
 
-    # Split the string to drop version and architecture
-    file_name = split('\.|-', file_name)
-
-    # Combine the name with prefix
-    file_name = file_name[0].lower() + '.' + file_name[-1]
-    file_path = context['file_dir'] + file_name
-
-    # Download the file if it does not exist and return the result
     if not exists(file_path):
-        return context.run(f"wget {url} -O {file_path}")
-    else:
-        return Result(f"File {file_path} already exists. Nothing to do.")
+        return context.run(f"wget {context['iso_url']} -O {file_path}")
+
+    return Result(f"File {file_path} already exists. Nothing to do.")
 
 @task
 def copy(context):
-    # Copy the files to the libvirt directory in order to prevent permission issues
-    context.run(f"sudo rsync -rog --chown={context['libvirt_user']}:{context['libvirt_user']} {context['file_dir']} {context['libvirt_dir']}")
+    """
+    Copies the files to libvirt directory via rsync.
+    It allows to avoid issues with permissions later on.
+    """
+    rsync_args = f"--chown={context['libvirt_user']} {context['file_dir']} {context['libvirt_dir']}"
+    return context.run("sudo rsync -rog " + rsync_args)
 
 namespace.add_task(get)
 namespace.add_task(copy)
