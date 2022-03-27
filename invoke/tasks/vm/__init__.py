f22251e60328802650bf3c35a9de0fc43f61e6da
Michał Żuber <michalzuber22@gmail.com>
2022-03-27 21:00:13 +0200

    Refactor vm.install task with pylint suggestions

--- invoke/tasks/vm/__init__.py
+++ invoke/tasks/vm/__init__.py
@@ -1,33 +1,40 @@
+"""
+Set of tasks managing the state of virtual machine
+"""
+import sys
+import libvirt
 from invoke import Collection, task
+from helpers import filename_from_url
+
+namespace = Collection()
 
 @task
-def install(context, config):
-    # Define the path to the vm disk file
-    disk_file = '/var/lib/libvirt/images/' + config['template']['variables']['hostname'] + '.' + config['vm-properties']['format']
+def install(context):
+    """
+    Installs a virtual machine in unattended way with preconfigured properties.
+    """
+    disk_path = context['libvirt']['dir'] + context['config']['hostname'] + '.qcow2'
+    iso_path = context['libvirt'].dir + filename_from_url(context['iso_url']) + '.iso'
+    config_name = filename_from_url(context['iso_url']) + '.ks'
+    config_path = context['libvirt'].dir + config_name
 
-    # Define location of the iso to be used
-    location = config['libvirt-images-dir'] + config['template']['name'] + '.iso'
+    install_command = f"""sudo virt-install \
+        --accelerate \
+        --check-cpu \
+        --connect=qemu:///system \
+        --disk {disk_path},size={context['libvirt']['vm']['disk_size']},format=qcow2 \
+        --extra-args="inst.ks=file:/{config_name}" \
+        --hvm \
+        --initrd-inject={config_path} \
+        --location={iso_path} \
+        --name {context['config']['hostname']} \
+        --network={context['libvirt']['vm']['network']} \
+        --noreboot \
+        --os-variant={context['libvirt']['vm']['os_variant']} \
+        --ram {context['libvirt']['vm']['ram']} \
+        --vcpus={context['libvirt']['vm'].vcpus} \
+        --wait -1
+    """
+    return context.run(install_command)
 
-    # Prepare kickstart injection parameters
-    if config['template']['output-ext'] == '.ks':
-        kickstart_name = config['template']['name'] + config['template']['output-ext']
-        extra_args = "inst.ks=file:/{}".format(kickstart_name)
-        initrd_inject = config['libvirt-images-dir'] + kickstart_name
-
-    install_command = "virt-install --accelerate --check-cpu --connect=qemu:///system --disk {},size={},format={} --extra-args=\"{}\" --hvm --initrd-inject={} --location={} --name {} --network={} --noreboot --os-variant={} --ram {} --vcpus={} --wait -1".format(
-            disk_file, 
-            config['vm-properties']['disk-size'],
-            config['vm-properties']['format'],
-            extra_args,
-            initrd_inject,
-            location,
-            config['template']['variables']['hostname'],
-            config['vm-properties']['network'],
-            config['vm-properties']['os-variant'],
-            config['vm-properties']['ram'],
-            config['vm-properties']['vcpus']
-    )
-    context.run(install_command)
-
-vm = Collection('vm')
-vm.add_task(install)
+namespace.add_task(install)
