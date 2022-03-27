271040ff89e9b23b3d470fb342006e98fd6818fc
Michał Żuber <michalzuber22@gmail.com>
2022-03-27 14:53:58 +0200

    Lint packages tasks

--- invoke/tasks/packages/__init__.py
+++ invoke/tasks/packages/__init__.py
@@ -1,10 +1,15 @@
+"""
+Set of tasks related to managing packages
+"""
 from invoke import Collection, task
 
 namespace = Collection()
 
 @task
 def install(context):
-    # Convert yaml list to string
+    """
+    Installs a list of packages defined in the config file
+    """
     packages_list = ' '.join(context['packages'])
     return context.run(f"sudo apt update && sudo apt install -y {packages_list}")
 
