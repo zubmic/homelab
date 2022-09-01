570aa524b09a493f5b68c0bb96c1568abb81b0cc
Michał Żuber <michalzuber22@gmail.com>
2022-09-01 09:08:31 +0200

    Add a script for VM installation with kickstart

---
+++ scripts/devlab_install.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+url='https://download.rockylinux.org/pub/rocky/9/isos/x86_64/Rocky-x86_64-minimal.iso'
+name='devlab'
+libvirt_path='/var/lib/libvirt/images'
+iso_path="$libvirt_path/rocky.iso"
+disk_path="$libvirt_path/$name.qcow2"
+config_path="$libvirt_path/$name.ks"
+
+domain=$(virsh list --all | grep $name)
+
+# Download the ISO only if it's not present already
+if [ ! -f $iso_path ]; then
+    wget $url -O $iso_path
+fi
+
+# Install VM if the domain doesn't already exist
+if [ -z "$domain" ]; then
+    cp ../files/"$name.ks" $config_path
+    virt-install \
+        --accelerate \
+        --check-cpu \
+        --connect=qemu:///system \
+        --disk $disk_path,size=20,format=qcow2 \
+        --extra-args="inst.ks=file:/$name.ks" \
+        --hvm \
+        --initrd-inject=$config_path \
+        --location=$iso_path \
+        --name $name \
+        --network=default \
+        --noreboot \
+        --os-variant=generic \
+        --ram 4096 \
+        --vcpus=2 \
+        --wait -1
+fi
+
+if [ ! -z "$domain" ]; then
+    virsh start $name
+    address=$(virsh domifaddr devlab | awk 'FNR == 3 { sub("/.*", ""); print $4 }')
+    echo "The address is: $address"
+fi
