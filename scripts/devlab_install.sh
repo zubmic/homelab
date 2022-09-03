b0b1f9afc384740fe8373a2444e3a1d21846f6e6
Michał Żuber <michalzuber22@gmail.com>
2022-09-03 06:51:37 +0200

    Change variable name for more readability

--- scripts/devlab_install.sh
+++ scripts/devlab_install.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 url='https://download.rockylinux.org/pub/rocky/9/isos/x86_64/Rocky-x86_64-minimal.iso'
-name='devlab'
+guest_vm_name='devlab'
 libvirt_path='/var/lib/libvirt/images'
 iso_path="$libvirt_path/rocky.iso"
-disk_path="$libvirt_path/$name.qcow2"
-config_path="$libvirt_path/$name.ks"
+disk_path="$libvirt_path/$guest_vm_name.qcow2"
+config_path="$libvirt_path/$guest_vm_name.ks"
 
-domain=$(virsh list --all | grep $name)
+domain=$(virsh list --all | grep $guest_vm_name)
+
+username='zubmic'
 
 # Download the ISO only if it's not present already
 if [ ! -f $iso_path ]; then
@@ -16,18 +18,18 @@ fi
 
 # Install VM if the domain doesn't already exist
 if [ -z "$domain" ]; then
-    cp ../files/"$name.ks" $config_path
+    cp ../files/"$guest_vm_name.ks" $config_path
 
     virt-install \
         --accelerate \
         --check-cpu \
         --connect=qemu:///system \
         --disk $disk_path,size=20,format=qcow2 \
-        --extra-args="inst.ks=file:/$name.ks" \
+        --extra-args="inst.ks=file:/$guest_vm_name.ks" \
         --hvm \
         --initrd-inject=$config_path \
         --location=$iso_path \
-        --name $name \
+        --name $guest_vm_name \
         --network=default \
         --noreboot \
         --os-variant=generic \
@@ -36,10 +38,9 @@ if [ -z "$domain" ]; then
         --wait -1
 
     # Start the VM
-    virsh start $name && sleep 5
+    virsh start $guest_vm_name && sleep 5
 
-    # Get the IP address
-    address=$(virsh domifaddr devlab | awk 'FNR == 3 { sub("/.*", ""); print $4 }')
-
-    echo "The address is: $address"
+    # Get the guest IP address
+    guest_ip=$(virsh domifaddr devlab | awk 'FNR == 3 { sub("/.*", ""); print $4 }')
+    echo "The guest address is: $guest_ip"
 fi
